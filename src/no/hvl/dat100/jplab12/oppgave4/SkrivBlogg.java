fbbac77135d07289abc7d727148b91947e842bc7
586600 <54099652+586600@users.noreply.github.com>
2019-11-10 16:13:27 +0100

    Add files via upload

--- src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
@@ -2,16 +2,34 @@ package no.hvl.dat100.jplab12.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.File;
 
 import no.hvl.dat100.jplab12.oppgave3.*;
-import no.hvl.dat100.jplab12.common.TODO;
 
 public class SkrivBlogg {
 
-	private static String MAPPE = System.getProperty("user.dir") + "/src/no/hvl/dat100/jplab12/tests/";
+	private static String FILEPATH = "/Users/iselinnilsen/OneDrive/HVL/DAT100/DAT100 Oblig 12/dat100-jplab12-testing/src/no/hvl/dat100/jplab12/tests/blogg.dat";
 
-	public static boolean skriv(Blogg samling, String filnavn) {
+	public static boolean skriv(Blogg samling, String filepath) {
+		
+		try {
+			
+			File file = new File(filepath);
+			PrintWriter skriver = new PrintWriter(file);
+			
+			String outputString = samling.toString();
+			
+			
+			skriver.println(outputString);
+			skriver.close();
+			
+			return true;
+			
+		} catch (FileNotFoundException e) {
+			System.out.print(e.toString());
+			return false;
+		}
 
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 }
